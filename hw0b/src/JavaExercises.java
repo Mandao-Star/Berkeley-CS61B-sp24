5b6d708ec8ed9e4a12dcb09aedecfcf7508ef73e
Mandao-Star <3966359781@qq.com>
2025-05-03 16:22:28 +0800

    第一次用IJ提交， JavaExercises

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,13 +1,17 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-//jjjjjjjjjjjjjjjjjj
+
 public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] array = new int[6];
+        for(int i = 0; i < 6; i++){
+            array[i] = i +1;
+        }
+        return array;
     }
 
     /** Returns the order depending on the customer.
@@ -15,15 +19,34 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+
+        if(customer == "Ergun"){
+            String[] result = new String[]{"beyti", "pizza", "hamburger", "tea"};
+            return result;
+        }else if(customer == "Erik"){
+            String[] result = new String[]{"sushi", "pasta", "avocado", "coffee"};
+            return result;
+        }else {
+            String[] result = new String[3];
+            return result;
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int max, min;
+        max = min = array[0];
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+        return max - min;
     }
 
     /**
@@ -39,8 +62,16 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+
+        if(x == 1){
+            return list;
+        }else if(x % 2 == 1){
+            return hailstoneHelper(3 * x + 1, list);
+        }else{
+            return hailstoneHelper(x / 2, list);
+        }
+
     }
 
 }
