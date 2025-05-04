0e64579334137a7e1741880c0712d13712c55a6e
Mandao-Star <3966359781@qq.com>
2025-05-04 16:43:07 +0800

    第一次在windows的powershell上提交， 提交hw0b

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    static int numDesserts = 0;
+    private int flavor;
+    private int price;
+    public Dessert(int f, int p)
+    {
+        flavor = f;
+        price = p;
+        numDesserts++;
+    }
+    public void printDessert(){
+        System.out.println(flavor + " "  + price + " " + numDesserts);
+    }
+
+    public  static void main(String[] args){
+        System.out.println("I love dessert!");
+    }
 }
